3ad62c1da5ccdf77d6068014e72ee229b854869e
JeffersonPoetaDuran <166729009+JeffersonPoetaDuran@users.noreply.github.com>
2024-04-24 08:41:22 -0400

    Delete javascript directory

--- javascript/js/javascript02.js
+++
@@ -1,8 +0,0 @@
-/*
-Descripcion: Ejemplo de mostrar color en consola
-Desarrollador: Jefferson Poeta Duran
-Fecha: 2024-04-15
-Cambios: Ningun cambio
-*/
-var color= prompt("Dime un color");
-console.log("El color es:", color)
